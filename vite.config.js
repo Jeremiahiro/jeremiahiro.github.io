57a4d44967429ea549478a9861cceca74b0a3bd6
jeremaihiro <jeremiahiro@gmail.com>
2024-01-03 08:44:09 +0100

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/jeremiahiro.github.io/'
+  base: '/jeremiahiro.github.io/',
+  plugins: [react()]
 })
