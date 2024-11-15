dca6429d12e09780027ea3ecd77d34be8ed0638d
jeremaihiro <jeremiahiro@gmail.com>
2024-11-15 22:52:37 +0100

    deploy prep

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jeremiahiro.github.io',
   integrations: [tailwind()],
+  site: 'https://jeremiahiro.github.io',
+  base: '/jeremiahiro.github.io',
 });
