96fd7f06c29549ba6516f88ccb3a74526f433333
jeremaihiro <jeremiahiro@gmail.com>
2025-02-08 04:04:23 +0100

    remove dist

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://jeremiahiro.github.io'
+  site: 'https://jeremiahiro.github.io',
+  output: 'static',
 });
